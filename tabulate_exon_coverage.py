453fd9aefd0a0e97854cfc56253324cc4b46a93d
xiuchengquek <biobirdman@gmail.com>
2015-11-16 19:36:29 +1100

    remove errornoeous arg

--- tabulate_exon_coverage.py
+++ tabulate_exon_coverage.py
@@ -111,7 +111,7 @@ if __name__ == '__main__' :
             line = line.strip()
             fields = line.split('\t')
             transcript_id = transcript_re.search(fields[9]).group(1)
-            transcript_manager.add_coverage(transcript_id, covered_base=covered_bases , length=exon_length)
+            transcript_manager.add_coverage(transcript_id, bases=fields[-1])
 
 
     with open(output_file, 'w+') as f:
